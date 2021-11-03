0d6ad9da43f4d84ea6ecee954a7a3571724594c5
Mahesh S <maheshs60@gmail.com>
2021-11-03 18:47:15 +0530

    npm version update

--- src/data/DataProvider.ts
+++ src/data/DataProvider.ts
@@ -4,104 +4,186 @@ import Pagination from './Pagination';
 import Sort from './Sort';
 
 export default class DataProvider extends Component {
-  private static counter = 0;
-  public id: string;
-  private _sort;
-  private _pagination;
-  private _models: Model[] = [];
-  private _keys: string[] | void = [];
-  private _totalCount: number;
-  public modelClass: Model;
-  public totalCountPromise: Promise<number>;
-
-  public constructor(config) {
-    super(config);
-    Object.assign(this, config);
-  }
-
-  public async init() {
-    await super.init.call(this);
-    if (this.id === null) {
-      if (DataProvider.counter > 0) this.id = `dp-${DataProvider.counter}`;
-      DataProvider.counter++;
+    /**
+     * Number of data providers on the current page. Used to generate unique IDs.
+     */
+    private static counter = 0;
+    /**
+     * An ID that uniquely identifies the data provider among all data providers.
+     * Generated automatically the following way in case it is not set:
+     *
+     * - First data provider ID is empty.
+     * - Second and all subsequent data provider IDs are: "dp-1", "dp-2", etc.
+     */
+    public id: string;
+    private _sort;
+    private _pagination;
+    private _models: Model[] = [];
+    private _keys: string[] | void = [];
+    private _totalCount: number;
+    /**
+     * Model Class which can be used for resolving Attribute Labels, Hints etc.
+     */
+    public modelClass: Model | undefined;
+    /**
+     * Promise which resolves to the number matching records for the current query.
+     */
+    public totalCountPromise: Promise<number>;
+    public constructor(config) {
+        super(config);
+        Object.assign(this, config);
     }
-  }
-
-  public async prepare(forcePrepare = false) {
-    if (forcePrepare || this._models.length === 0) {
-      this._models = await this.prepareModels();
+    /** @inheritdoc */
+    public async init() {
+        await super.init.call(this);
+        if (this.id === null) {
+        if (DataProvider.counter > 0) this.id = `dp-${DataProvider.counter}`;
+        DataProvider.counter++;
+        }
     }
-    if (forcePrepare || (this._keys as Array<string>).length === 0) {
-      this._keys = this.prepareKeys(this._models);
+    /**
+     * Prepares the data models and keys.
+     *
+     * This method will prepare the data models and keys that can be retrieved via
+     * [[getModels]] and [[getKeys]].
+     *
+     * This method will be implicitly called by [[getModels]] and [[getKeys]] if it has not been called before.
+     *
+     * @param forcePrepare whether to force data preparation even if it has been done before.
+     */
+    public async prepare(forcePrepare = false) {
+        if (forcePrepare || this._models.length === 0) {
+        this._models = await this.prepareModels();
+        }
+        if (forcePrepare || (this._keys as Array<string>).length === 0) {
+        this._keys = this.prepareKeys(this._models);
+        }
     }
-  }
-
-  public prepareKeys(models: Model[]) {}
-
-  public async prepareModels(): Promise<Model[]> {
-    return new Promise<Model[]>((resolve, reject) => {});
-  }
-
-  public async prepareTotalCount(): Promise<number> {
-    return 0;
-  }
-
-  public async getModels(): Promise<Model[]> {
-    await this.prepare();
-    return this._models;
-  }
-
-  public setModels(models: Model[]) {
-    this._models = models;
-  }
-
-  public getKeys() {
-    return this._keys;
-  }
-
-  public getCount() {
-    return this._models.length;
-  }
-
-  public async getTotalCount() {
-    if (this._totalCount === undefined) this._totalCount = await this.prepareTotalCount();
-
-    return this._totalCount;
-  }
-
-  public setTotalCount(value: number) {
-    this._totalCount = value;
-  }
-
-  public getPagination() {
-    if (this._pagination === undefined) this.setPagination({});
-    return this._pagination;
-  }
-
-  public setPagination(value) {
-    if (value instanceof Pagination || value === false) this._pagination = value;
-    else this._pagination = new Pagination(value);
-  }
-
-  public getSort() {
-    if (this._sort === undefined) this.setSort([]);
-    return this._sort;
-  }
-
-  public setSort(value) {
-    if (value instanceof Sort || value === false) this._sort = value;
-    else {
-      const config: any = {};
-
-      if (this.id !== undefined) config.sortParam = `${this.id}-sort`;
-      this._sort = new Sort({ ...value, ...config });
-      // this._sort.enableMultiSort = true;
+    /**
+     * Prepares the keys associated with the currently available data models.
+     * @param models the available data models
+     * @return the keys
+     */
+    public prepareKeys(models: Model[]) {}
+    /**
+     * Prepares the data models that will be made available in the current page.
+     * @return the available data models
+     */
+    public async prepareModels(): Promise<Model[]> {
+        return new Promise<Model[]>((resolve, reject) => {});
     }
-  }
-
-  public refresh() {
-    this._totalCount = null;
-    this._models = null;
-    this._keys = null;
-  }
+    /**
+     * Returns a value indicating the total number of data models in this data provider.
+     * @return total number of data models in this data provider.
+     */
+    public async prepareTotalCount(): Promise<number> {
+        return 0;
+    }
+    /**
+     * Returns the data models in the current page.
+     * @return the list of data models in the current page.
+     */
+    public async getModels(): Promise<Model[]> {
+        await this.prepare();
+        return this._models;
+    }
+    /**
+     * Sets the data models in the current page.
+     * @param models the models in the current page
+     */
+    public setModels(models: Model[]) {
+        this._models = models;
+    }
+    /**
+     * Returns the key values associated with the data models.
+     * @return the list of key values corresponding to [[models]]. Each data model in [[models]]
+     * is uniquely identified by the corresponding key value in this array.
+     */
+    public getKeys() {
+        return this._keys;
+    }
+    /**
+     * Returns the number of data models in the current page.
+     * @return the number of data models in the current page.
+     */
+    public getCount() {
+        return this._models.length;
+    }
+    /**
+     * Returns the total number of data models.
+     * When [[pagination]] is false, this returns the same value as [[count]].
+     * Otherwise, it will call [[prepareTotalCount]] to get the count.
+     * @return total number of possible data models.
+     */
+    public async getTotalCount() {
+        if (this._totalCount === undefined) this._totalCount = await this.prepareTotalCount();
+        return this._totalCount;
+    }
+    /**
+     * Sets the total number of data models.
+     * @param value the total number of data models.
+     */
+    public setTotalCount(value: number) {
+        this._totalCount = value;
+    }
+    /**
+     * Returns the pagination object used by this data provider.
+     * Note that you should call [[prepare]] or [[getModels]] first to get correct values
+     * of [[Pagination.totalCount]] and [[Pagination::pageCount]].
+     * @return the pagination object. If this is false, it means the pagination is disabled.
+     */
+    public getPagination() {
+        if (this._pagination === undefined) this.setPagination({});
+        return this._pagination;
+    }
+    /**
+     * Sets the pagination for this data provider.
+     * @param value the pagination to be used by this data provider.
+     * This can be one of the following:
+     *
+     * - a configuration array for creating the pagination object. The "class" element defaults
+     *   to [[Pagination]]
+     * - an instance of [[Pagination]] or its subclass
+     * - false, if pagination needs to be disabled.
+     */
+    public setPagination(value) {
+        if (value instanceof Pagination || value === false) this._pagination = value;
+        else this._pagination = new Pagination(value);
+    }
+    /**
+     * Returns the sorting object used by this data provider.
+     * @return the sorting object. If this is false, it means the sorting is disabled.
+     */
+    public getSort() {
+        if (this._sort === undefined) this.setSort([]);
+        return this._sort;
+    }
+    /**
+     * Sets the sort definition for this data provider.
+     * @param value the sort definition to be used by this data provider.
+     * This can be one of the following:
+     *
+     * - a configuration array for creating the sort definition object. The "class" element defaults
+     *   to 'yii\data\Sort'
+     * - an instance of [[Sort]] or its subclass
+     * - false, if sorting needs to be disabled.
+     */
+    public setSort(value) {
+        if (value instanceof Sort || value === false) this._sort = value;
+        else {
+            const config: any = {};
+            if (this.id !== undefined) config.sortParam = `${this.id}-sort`;
+            this._sort = new Sort({ ...value, ...config });
+        }
+    }
+    /**
+     * Refreshes the data provider.
+     * After calling this method, if [[getModels]], [[getKeys]] or [[getTotalCount]] is called again,
+     * they will re-execute the query and return the latest data available.
+     */
+    public refresh() {
+        this._totalCount = null;
+        this._models = null;
+        this._keys = null;
+    }
 }
--- src/grid/GridView.ts
+++ src/grid/GridView.ts
@@ -129,11 +129,22 @@ export default class GridView extends CollectionView {
      * ```js
      * [
      *     'id',
-     *     'amount:currency:Total Amount',
-     *     'created_at:datetime',
+     *     'amount:text:Total Amount',
+     *     'created_at:text',
      * ]
      * ```
-     *
+     * Note: Format is now restricted to 'text' only. You can convert the data to a specific format by using
+     * callbacks as of now:
+     * 
+     * [
+     *     'id',
+     *     'title:text:Company Name',
+     *     {
+     *         title: 'Created Date',
+     *         value: (model, attribute) => (new Date(form.startDate.value)).toString('dd MMMM yyyy')
+     *     }
+     * ]
+     * 
      * When using a [[dataProvider]] with active records, you can also display values from related records,
      * e.g. the `name` attribute of the `author` relation:
      *
@@ -148,28 +159,91 @@ export default class GridView extends CollectionView {
      * ```
      */
     public columns: Array<column | string>;
+    /**
+     * The HTML display when the content of a cell is empty.
+     * This property is used to render cells that have no defined content,
+     * e.g. empty footer or filter cells.
+     */
     public emptyCell = '&nbsp;';
+    /**
+     * The model that keeps the user-entered filter data. When this property is set,
+     * the grid view will enable column-based filtering. Each data column by default will display a text field
+     * at the top that users can fill in to filter the data.
+     *
+     * Note that in order to show an input field for filtering, a column must have its [[DataColumn.attribute]]
+     * property set and the attribute should be active in the current scenario of $filterModel or have
+     * [[DataColumn.filter]] set as the HTML code for the input field.
+     *
+     * When this property is not set (null) the filtering feature is disabled.
+     */
     public filterModel: Model;
+    /**
+     * The URL for returning the filtering result. [[Url.to]] will be called to
+     * normalize the URL. If not set, the current controller action will be used.
+     * When the user makes change to any filter input, the current filtering inputs will be appended
+     * as GET parameters to this URL.
+     */
     public filterUrl: string;
+    /**
+     * Additional jQuery selector for selecting filter input fields
+     */
     public filterSelector: string;
+    /**
+     * Whether the filters should be displayed in the grid view. Valid values include:
+     *
+     * - header: the filters will be displayed on top of each column's header cell.
+     * - body: the filters will be displayed right below each column's header cell.
+     * - footer: the filters will be displayed below each column's footer cell.
+     */
     public filterPosition = 'body';
+    /**
+     * The HTML attributes for the filter row element.
+     * @see [[Html::renderTagAttributes]] for details on how attributes are being rendered.
+     */
     public filterRowOptions: { [key: string]: any } = { class: 'filters' };
+    /**
+     * The options for rendering the filter error summary.
+     * Please refer to [[Html.errorSummary]] for more details about how to specify the options.
+     * @see [[renderErrors]]
+     */
     public filterErrorSummaryOptions: { [key: string]: any } = { class: 'error-summary' };
+    /**
+     * The options for rendering every filter error message.
+     * This is mainly used by [[Html.error]] when rendering an error message next to every filter input field.
+     */
     public filterErrorOptions: { [key: string]: any } = { class: 'help-block' };
+    /**
+     * Whatever to apply filters on losing focus. Leaves an ability to manage filters via gridView JS
+     */
     public filterOnfocusOut = true;
+    /**
+     * The layout that determines how different sections of the grid view should be organized.
+     * The following tokens will be replaced with the corresponding section contents:
+     *
+     * - `{summary}`: the summary section. See [[renderSummary()]].
+     * - `{errors}`: the filter model error summary. See [[renderErrors()]].
+     * - `{items}`: the list items. See [[renderItems()]].
+     * - `{sorter}`: the sorter. See [[renderSorter()]].
+     * - `{pager}`: the pager. See [[renderPager()]].
+     */
     public layout = '{summary}\n{items}\n{pager}';
 
     public constructor(config: { [key: string]: any }) {
         super(config);
         Object.assign(this, config);
     }
-
+    /**
+     * Initializes the grid view.
+     * This method will initialize required property values and instantiate [[columns]] objects.
+     */
     public async init() {
         await super.init.call(this);
         if (this.filterRowOptions.id === undefined) this.filterRowOptions.id = `${this.options.id}-filters`;
         await this.initColumns();
     }
-
+    /**
+     * Runs the widget.
+     */
     public async run(): Promise<string> {
         await Pwoli.view.publishAndRegisterFile(path.join(__dirname, '/../assets/js/gridView.js'));
         await Pwoli.view.publishAndRegisterFile(path.join(__dirname, '/../assets/css/bootstrap.css'));
@@ -179,15 +253,14 @@ export default class GridView extends CollectionView {
         await Pwoli.view.registerJs(`jQuery('#${id}').pwoliGridView(${JSON.stringify(options)});`);
         return await super.run.call(this);
     }
-
+    /**
+     * Renders validator errors of filter model.
+     * @return the rendering result.
+     */
     public renderErrors() {
-        return '';
+        return ''; //coming soon..
     }
-
-    public async render() {
-        return await super.render.call(this);
-    }
-
+    /** @inheritdoc */
     public async renderSection(name: string): Promise<string> {
         switch (name) {
         case '{errors}':
@@ -196,7 +269,10 @@ export default class GridView extends CollectionView {
             return await super.renderSection.call(this, name);
         }
     }
-
+    /**
+     * Returns the options for the grid view JS widget.
+     * @return the options
+     */
     protected getClientOptions(): { filterUrl: string; filterSelector: string } {
         const filterUrl = this.filterUrl !== undefined ? this.filterUrl : Pwoli.request.originalUrl || Pwoli.request.url;
         const id = this.filterRowOptions.id;
@@ -204,7 +280,10 @@ export default class GridView extends CollectionView {
         if (this.filterSelector !== undefined) filterSelector += `, ${this.filterSelector}`;
         return { filterUrl, filterSelector };
     }
-
+    /**
+     * Renders the data models for the grid view.
+     * @return the HTML code of table
+     */
     public async renderItems(): Promise<string> {
         const caption = this.renderCaption();
         const columnGroup = await this.renderColumnGroup();
@@ -220,23 +299,32 @@ export default class GridView extends CollectionView {
         );
         return Html.tag('table', content.join('\n'), this.tableOptions);
     }
-
+    /**
+     * Renders the caption element.
+     * @return the rendered caption element or `false` if no caption element should be rendered.
+     */
     public renderCaption(): string | false {
         if (this.caption.length > 0) return Html.tag('caption', this.caption, this.captionOptions);
         return false;
     }
-
+    /**
+     * Renders the column group HTML.
+     * @return the column group HTML or `false` if no column group should be rendered.
+     */
     public async renderColumnGroup(): Promise<string | false> {
         for (const column of this.columns) {
-        if ((column as column).options !== undefined && (column as column).options.length > 0) {
-            const cols = [];
-            for (const col of this.columns) cols.push(Html.tag('col', '', (col as column).options));
-            return Html.tag('colgroup', cols.join('\n'));
-        }
+            if ((column as column).options !== undefined && (column as column).options.length > 0) {
+                const cols = [];
+                for (const col of this.columns) cols.push(Html.tag('col', '', (col as column).options));
+                return Html.tag('colgroup', cols.join('\n'));
+            }
         }
         return false;
     }
-
+    /**
+     * Renders the table header.
+     * @return the rendering result.
+     */
     public async renderTableHeader(): Promise<string> {
         const cells = [];
         for (const column of this.columns) cells.push(await (column as column).renderHeaderCell());
@@ -246,7 +334,10 @@ export default class GridView extends CollectionView {
 
         return `<thead>\n${content}\n</thead>`;
     }
-
+    /**
+     * Renders the table footer.
+     * @return the rendering result.
+     */
     public async renderTableFooter(): Promise<string> {
         const cells = [];
         for (const column of this.columns) cells.push(await (column as column).renderFooterCell());
@@ -254,7 +345,10 @@ export default class GridView extends CollectionView {
         if (this.filterPosition === 'footer') content = await this.renderFilters();
         return `<tfoot>\n${content}\n</tfoot>`;
     }
-
+    /**
+     * Renders the filter.
+     * @return the rendering result.
+     */
     public async renderFilters(): Promise<string> {
         if (this.filterModel !== undefined) {
         const cells = [];
@@ -263,7 +357,10 @@ export default class GridView extends CollectionView {
         }
         return '';
     }
-
+    /**
+     * Renders the table body.
+     * @return the rendering result.
+     */
     public async renderTableBody(): Promise<string> {
         const models = this._models;
         const keys = this.dataProvider.getKeys();
@@ -280,19 +377,24 @@ export default class GridView extends CollectionView {
         return `<tbody>\n<tr><td colspan="${this.columns.length}">${this.renderEmpty()}</td></tr>\n</tbody>`;
         return `<tbody\n${rows.join('\n')}\n</tbody>`;
     }
-
-    public async renderTableRow(model, key, index): Promise<string> {
+    /**
+     * Renders a table row with the given data model and key.
+     * @param model the data model to be rendered
+     * @param key the key associated with the data model
+     * @param index the zero-based index of the data model among the model array returned by [[dataProvider]].
+     * @return the rendering result
+     */
+    public async renderTableRow(model: Model, key: string, index: number): Promise<string> {
         const cells = [];
         for (const column of this.columns) cells.push(await (column as column).renderDataCell(model, key, index));
         const options = this.rowOptions;
         options['data-key'] = key;
-
         return Html.tag('tr', cells.join(''), options);
     }
-
+    /**
+     * Creates column objects and initializes them.
+     */
     public async initColumns() {
-        // await this.initialization;
-
         if (this.columns.length === 0) this.guessColumns();
         let i = 0;
         for (let column of this.columns) {
@@ -309,21 +411,29 @@ export default class GridView extends CollectionView {
         i++;
         }
     }
-
+    /**
+     * Creates a [[DataColumn]] object based on a string in the format of "attribute:format:label".
+     * @param text the column specification string
+     * @return the column instance
+     * @throws Error if the column specification is invalid
+     */
     protected createDataColumn(text: string) {
         const matches = text.match(/^([^:]+)(:(\w*))?(:(.*))?$/);
         if (matches.length === 0)
-        throw new Error(
-            'The column must be specified in the format of "attribute", "attribute:format" or "attribute:format:label"',
-        );
+            throw new Error(
+                'The column must be specified in the format of "attribute", "attribute:format" or "attribute:format:label"',
+            );
         return new this.dataColumnClass({
-        grid: this,
-        attribute: matches[1],
-        format: matches[3] !== undefined ? matches[3] : 'text',
-        label: matches[5] !== undefined ? matches[5] : undefined,
+            grid: this,
+            attribute: matches[1],
+            format: matches[3] !== undefined ? matches[3] : 'text',
+            label: matches[5] !== undefined ? matches[5] : undefined,
         });
     }
-
+    /**
+     * This function tries to guess the columns to show from the given data
+     * if [[columns]] are not explicitly specified.
+     */
     protected guessColumns() {
         const models = this._models;
         const model = models[1];
