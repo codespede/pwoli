23c7e7113a8a1d89c0f2b26d8960edb8926506f6
Mahesh S <maheshs60@gmail.com>
2021-11-04 17:55:05 +0530

    moved existing docs to api-docs

--- src/base/Application.ts
+++ src/base/Application.ts
@@ -3,19 +3,66 @@ import SequelizeAdapter from '../orm-adapters/SequelizeAdapter';
 import View from './View';
 import Serializer from '../rest/Serializer';
 import url = require('url');
+/**
+ * Application is the base class for all web application classes.
+ *
+ * For more details and usage information on Application, see the [guide article on applications](guide:structure-applications).
+ *
+ * @property-read ErrorHandler $errorHandler The error handler application component. This property is
+ * read-only.
+ * @property string $homeUrl The homepage URL.
+ * @property-read Request $request The request component. This property is read-only.
+ * @property-read Response $response The response component. This property is read-only.
+ * @property-read Session $session The session component. This property is read-only.
+ * @property-read User $user The user component. This property is read-only.
+ *
+ * @author Mahesh S Warrier <https://github.com/codespede>
+ */
 export default class Application extends Component {
     public static request: { [key: string]: any };
     public static response: { [key: string]: any } = { data: null, headers: {}, status: null };
-    public static charset = 'UTF-8';
+    /**
+     * The [[View]] object for the application that is used to render various view files..
+     */
     public static view = new View({});
+    /**
+     * @var registered path aliases
+     * @see [[getAlias]]
+     * @see [[setAlias]]
+     */
     public static aliases = {};
+    /**
+     * The directory that contains the view files for this module.
+     */
     public static viewPath: string;
+    /**
+     * The ORM that should be used by the application. Defaults to 'sequelize'
+     * Any other ORM if used should have an [[ORMAdapter]] defined in [[ormAdapterClasses]]
+     */
     public static orm: 'sequelize' | 'other' = 'sequelize';
+    /**
+     * The adapters for any ORMs used by this application.
+     * The key of the adapter to be used should be specified in [[orm]]
+     * By default, support for Sequelize ORM is provided.
+     */
     public static ormAdapterClasses = { sequelize: SequelizeAdapter };
     private static _ormAdapter;
+    /**
+     * The model class for the ORM being used. Eg:- [[sequelize.Model]] for Sequelize
+     * This should be set in the entry script of the application like:
+     * ```js
+     * import { DataTypes, Model } from 'sequelize';
+     * Application.ormModelClass = Model;
+     * ```
+     */
     public static ormModelClass;
+    /**
+     * The [[Serializer]] that should be used for RESTful operations.
+     */
     public static serializer = new Serializer({});
-
+    /**
+     * Returns the full URL of the request.
+     */
     public static getFullUrlOfRequest() {
         const req = this.request;
         return decodeURIComponent(
@@ -26,29 +73,49 @@ export default class Application extends Component {
             }),
         );
     }
-
+    /**
+     * Registers a path alias.
+     *
+     * A path alias is a short name representing a long path (a file path, a URL, etc.)
+     */
     public static setAlias(alias, path) {
         this.aliases[alias] = path;
     }
-
-    public static getAlias(alias) {
+    /**
+     * Translates a path alias into an actual path.
+     * @param alias The alias to be translated.
+     * @return The translated alias.
+     * @see [[setAlias]]
+     */
+    public static getAlias(alias: string) {
         return this.aliases[alias] !== undefined ? this.aliases[alias] : false;
     }
-
-    public static setViewPath(path) {
+    /**
+     * Sets the viewPath.
+     * @param path The path to be set.
+     */
+    public static setViewPath(path: string) {
         const resolved = this.getAlias(path);
         this.viewPath = resolved !== false ? resolved : path;
     }
-
+    /**
+     * Returns the viewPath.
+     * @return The actual viewPath.
+     */
     public static getViewPath() {
         return this.viewPath;
     }
-
+    /**
+     * Returns the viewPath.
+     * @return The actual viewPath.
+     */
     public static getORMAdapter() {
         if (this._ormAdapter === undefined) this._ormAdapter = new this.ormAdapterClasses[this.orm]({});
         return this._ormAdapter;
     }
-
+    /**
+     * Responds to the client(eg:- browser) depending on the data.
+     */
     public static async respond(nativeResponse, data = null) {
         //console.log('ares', data);
         if (typeof data === 'string') this.response.data = data;
@@ -66,7 +133,9 @@ export default class Application extends Component {
         nativeResponse.write(this.response.data);
         nativeResponse.end();
     }
-
+    /**
+     * Adds necessary headers and status code to the given response.
+     */
     public static responsify(response) {
         for (let header in this.response.headers) response.setHeader(header, this.response.headers[header] || '');
         if (this.response.status !== null) response.status = this.response.status;
